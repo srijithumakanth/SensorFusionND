067ecea3fd32f3fdf6e878cf1ea12363bf61a1c5
srijith <sumakanthil@innovative-logistics.com>
2020-05-28 23:15:06 -0500

    2D Eucleadian clustering from scratch

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/cluster.cpp
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/cluster.cpp
@@ -75,13 +75,45 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+void clusterHelper (int id, const std::vector<std::vector<float>>& points, std::vector<int>& cluster, std::vector<bool>& processed, KdTree* tree, float distanceTol)
+{
+	processed[id] = true;
+	cluster.push_back(id);
+
+	// Nearby points
+	std::vector<int> nearby = tree->search(points[id], distanceTol);
+
+	for (int i : nearby)
+	{
+		if (!processed[i])
+			clusterHelper(i, points, cluster, processed, tree, distanceTol);
+	}
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 
 	// TODO: Fill out this function to return list of indices for each cluster
 
 	std::vector<std::vector<int>> clusters;
- 
+	
+	std::vector<bool> processed(points.size(), false);
+
+	int i = 0;
+	while (i < points.size())
+	{
+		if (processed[i])
+		{
+			i++;
+			continue;
+		}
+
+		std::vector<int> cluster;
+		clusterHelper (i, points, cluster, processed, tree, distanceTol);
+		clusters.push_back(cluster);
+		i++;
+	}
+
 	return clusters;
 
 }
