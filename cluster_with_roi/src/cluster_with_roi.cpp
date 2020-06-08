a6154a8378baba42d9d35c8898a1433fcdd3123b
srijith <sumakanthil@innovative-logistics.com>
2020-06-08 00:17:59 -0500

    Remove multiple bounding boxes over the same 3D object

--- cluster_with_roi/src/cluster_with_roi.cpp
+++ cluster_with_roi/src/cluster_with_roi.cpp
@@ -109,12 +109,21 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
             // check wether point is within current bounding box
             if (smallerBox.contains(pt))
             {
-                it2->lidarPoints.push_back(*it1);
-                lidarPoints.erase(it1);
-                it1--;
-                break;
+                // it2->lidarPoints.push_back(*it1);
+                // lidarPoints.erase(it1);
+                // it1--;
+                // break;
+                enclosingBoxes.push_back(it2);
             }
         } // eof loop over all bounding boxes
+
+        // check wether point has been enclosed by one or by multiple boxes
+        if (enclosingBoxes.size() == 1)
+        {
+            // add LiDAR point to the bounding box
+            enclosingBoxes[0]->lidarPoints.push_back(*it1);
+        }
+
 
     } // eof loop over all Lidar points
 }
