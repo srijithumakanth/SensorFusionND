ae50c0bafd605fa17fefefecd70d69bb20f32ba6
srijith <sumakanthil@innovative-logistics.com>
2020-06-20 22:38:58 -0500

    Sigma point prediction

--- Kalman_Filters/UKF/main.cpp
+++ Kalman_Filters/UKF/main.cpp
@@ -4,23 +4,30 @@
 
 using Eigen::MatrixXd;
 
-int main() {
+int main() 
+{
 
-  // Create a UKF instance
-  UKF ukf;
+    // Create a UKF instance
+    UKF ukf;
 
-  /**
-   * Programming assignment calls
-   */
-//   MatrixXd Xsig = MatrixXd(5, 11);
-//   ukf.GenerateSigmaPoints(&Xsig);
-  
-  /* Call for Augmented process noise model */
-  MatrixXd Xsig_aug = MatrixXd(7, 15);
-  ukf.AugmentedSigmaPoints(&Xsig_aug);
+    /*
+        Programming assignment calls
+    */
 
-  // print result
-//   std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+    /* Call for Generating sigma points */
+    //   MatrixXd Xsig = MatrixXd(5, 11);
+    //   ukf.GenerateSigmaPoints(&Xsig);
 
-  return 0;
+    /* Call for Augmented process noise model */
+    // MatrixXd Xsig_aug = MatrixXd(7, 15);
+    // ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+    /* Call for Sigma point prediction */
+    MatrixXd Xsig_pred = MatrixXd(15, 5);
+    ukf.SigmaPointPrediction(&Xsig_pred);
+
+    // print result
+    //   std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+    return 0;
 }
