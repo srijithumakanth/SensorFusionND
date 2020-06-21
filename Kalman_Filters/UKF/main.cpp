7a8ad10db49c49e94085e315bf3b71f2fc86e1ac
srijith <sumakanthil@innovative-logistics.com>
2020-06-20 22:10:53 -0500

    With augmented process model noise

--- Kalman_Filters/UKF/main.cpp
+++ Kalman_Filters/UKF/main.cpp
@@ -12,11 +12,15 @@ int main() {
   /**
    * Programming assignment calls
    */
-  MatrixXd Xsig = MatrixXd(5, 11);
-  ukf.GenerateSigmaPoints(&Xsig);
+//   MatrixXd Xsig = MatrixXd(5, 11);
+//   ukf.GenerateSigmaPoints(&Xsig);
+  
+  /* Call for Augmented process noise model */
+  MatrixXd Xsig_aug = MatrixXd(7, 15);
+  ukf.AugmentedSigmaPoints(&Xsig_aug);
 
   // print result
-  std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+//   std::cout << "Xsig = " << std::endl << Xsig << std::endl;
 
   return 0;
 }
